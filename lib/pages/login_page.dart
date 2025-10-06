2f26f5c1203d815653296d089301f35e2f7b8380
455eaguilar <455eaguilar@student.angelina.edu>
2025-10-06 21:24:18 +0000

    feat: add stateless widget for MyText

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key})
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(233, 6, 205, 185),
+      body: Center(
+        child: Column (
+          children: [
+            const SizedBox(height: 50),
+            const Icon(Icons.lock,size: 100),
+            const SizedBox(height: 25)
+            const Text("Hello again, we've missed you!"),
+            const SizedBox(height: 25 )
+          ],
+        ),
+      ),
+    );
+  }
+}
