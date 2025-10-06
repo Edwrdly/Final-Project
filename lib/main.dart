9104d3ea9ebd832586d6f37fb1ac90b62c76a1f1
455eaguilar <455eaguilar@student.angelina.edu>
2025-10-06 17:45:57 +0000

    feat: add stateful widget for LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  
+  const MyApp({super.key})
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
