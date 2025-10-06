89edb112038457c143f0e76485007b4e930684b5
455eaguilar <455eaguilar@student.angelina.edu>
2025-10-06 22:22:48 +0000

    feat: add button.dart file

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../components/text_field.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -7,20 +8,38 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
+final emailTextController = TextEditingController();
+final passwordTextController = TextEditingController();
+
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(233, 6, 205, 185),
+      backgroundColor: Colors.teal,
       body: Center(
-        child: Column (
-          children: [
-            const SizedBox(height: 50),
-            const Icon(Icons.lock,size: 100),
-            const SizedBox(height: 25)
-            const Text("Hello again, we've missed you!"),
-            const SizedBox(height: 25 )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column (
+            children: [
+              const SizedBox(height: 50),
+              const Icon(Icons.lock,size: 100),
+              const SizedBox(height: 25),
+              const Text("Hello again, we've missed you!"),
+              const SizedBox(height: 25),
+              MyTextField(
+                controller: emailTextController,
+                hintText: 'Email',
+                obscureText: false
+              ),
+              
+              const SizedBox(height: 10),
+              MyTextField(
+                controller: passwordTextController,
+                hintText: 'Password',
+                obscureText: true, 
+              ),
+            ],
+          ),
         ),
       ),
     );
